32c3222a5ad7501c35e3bde667b6c75820390310
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-18 09:19:24 -0300

    aula: operadores unarios

---
+++ 01-fundamentos_do_java/meu-projeto/src/je06_atribuicoes/Unarios.java
@@ -0,0 +1,21 @@
+package je06_atribuicoes;
+
+public class Unarios {
+
+    public static void main(String[] args) {
+
+        int numero = +5; //ou somente 5
+
+        //Imprimindo o numero negativo
+        System.out.println(-numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero++;
+        System.out.println(numero);
+
+        boolean verdadeira = true;
+        System.out.println(!verdadeira); // false
+
+    }
+
+}
