9c9eb3225ae03215ebeb4a82df11fcf1d373652d
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-21 08:25:54 -0300

    aula: comentarios

---
+++ 01-fundamentos_do_java/meu-projeto/src/je06_atribuicoes/Comentarios.java
@@ -0,0 +1,17 @@
+package je06_atribuicoes;
+
+public class Comentarios {
+
+    public static void main(String[] args) {
+
+        // Linha de comentário única
+
+        /*
+        Bloco de comentário
+        podendo colocar
+        diversas linhas
+         */
+
+    }
+
+}
