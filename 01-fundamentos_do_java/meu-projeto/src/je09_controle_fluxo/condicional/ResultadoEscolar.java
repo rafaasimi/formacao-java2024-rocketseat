3b761a71edce4ca1d17a5b980821a5a94fcea153
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-22 08:51:03 -0300

    aula: atribuição ternária

--- 01-fundamentos_do_java/meu-projeto/src/je09_controle_fluxo/condicional/ResultadoEscolar.java
+++ 01-fundamentos_do_java/meu-projeto/src/je09_controle_fluxo/condicional/ResultadoEscolar.java
@@ -4,15 +4,21 @@ public class ResultadoEscolar {
 
     public static void main(String[] args) {
 
-        double nota = 5;
+        // if-else
+//        double nota = 5;
+//
+//        if (nota >= 7) {
+//            System.out.println("Aprovado");
+//        } else if (nota >= 5 && nota < 7) {
+//            System.out.println("Recuperação");
+//        } else {
+//            System.out.println("Reprovado");
+//        }
 
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Recuperação");
-        } else {
-            System.out.println("Reprovado");
-        }
+        // ternário
+        double nota = 6;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
 
     }
 
