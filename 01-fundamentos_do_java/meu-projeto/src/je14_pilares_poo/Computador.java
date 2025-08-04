ff8a9e69dfd5d5ada8e8f0d3e704ba6517261d42
Rafael Simionato <rafaasimi@hotmail.com>
2025-08-04 09:30:49 -0300

    aula: encapsulamento

---
+++ 01-fundamentos_do_java/meu-projeto/src/je14_pilares_poo/Computador.java
@@ -0,0 +1,16 @@
+package je14_pilares_poo;
+
+public class Computador {
+
+    public static void main(String[] args) {
+
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+//        msn.validarConectadoInternet();
+//        msn.salvarHistoricoMensagem();
+
+    }
+
+}
