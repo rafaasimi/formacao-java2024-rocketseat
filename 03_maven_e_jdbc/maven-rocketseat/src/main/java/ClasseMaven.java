ee2c8f019e6784bfd79e743ddb2c90f47a4ae99a
Rafael Simionato <rafaasimi@hotmail.com>
2025-12-30 18:56:51 -0300

    feat: alterado estrutura pastas

--- 03_maven_e_jdbc/maven-rocketseat/src/main/java/ClasseMaven.java
+++
@@ -1,9 +0,0 @@
-public class ClasseMaven {
-
-    public static void main(String[] args) {
-
-        System.out.println("Olá, Maven!");
-
-    }
-
-}
---
+++ 04_fundamentos_do_spring_boot/primeiro-projeto-spring-boot/src/test/java/dev/rafaelsimionato/primeiro_projeto_spring_boot/PrimeiroProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelsimionato.primeiro_projeto_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
