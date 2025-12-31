ba17c367403c39471282bfb4d3f89f6417fa5516
Rafael Simionato <rafaasimi@hotmail.com>
2025-12-31 10:48:13 -0300

    aula: component scan

--- 04_fundamentos_do_spring_boot/primeiro-projeto-spring-boot/src/main/java/dev/rafaelsimionato/primeiro_projeto_spring_boot/PrimeiroProjetoSpringBootApplication.java
+++ 04_fundamentos_do_spring_boot/primeiro-projeto-spring-boot/src/main/java/dev/rafaelsimionato/primeiro_projeto_spring_boot/PrimeiroProjetoSpringBootApplication.java
@@ -2,8 +2,12 @@ package dev.rafaelsimionato.primeiro_projeto_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.management.MXBean;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "dev.rafaelsimionato")
 public class PrimeiroProjetoSpringBootApplication {
 
     public static void main(String[] args) {
