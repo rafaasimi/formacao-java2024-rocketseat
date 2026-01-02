b26cc971508dc3a0c79a4bd72d3ceb6d68e6e3c0
Rafael Simionato <rafaasimi@hotmail.com>
2026-01-02 09:10:04 -0300

    aula: formatando mensagem de validacao

---
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,35 @@
+package dev.rafaelsimionato.gestao_vagas.exceptions;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    private MessageSource messageSource;
+
+    public ExceptionHandlerController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+            dto.add(new ErrorMessageDTO(message, error.getField()));
+        });
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+}
