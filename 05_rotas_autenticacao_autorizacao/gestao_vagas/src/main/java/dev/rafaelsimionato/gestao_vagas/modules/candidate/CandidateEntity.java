e323f94e4b075745e9385673faa6c1af1207c6c4
Rafael Simionato <rafaasimi@hotmail.com>
2026-01-02 10:41:43 -0300

    aula: criando tabela de candidato

--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/CandidateEntity.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,29 @@
 package dev.rafaelsimionato.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+//    Caso eu queira que o nome da coluna seja diferente do nome do atributo, descomente a linha abaixo
+//    @Column(name = "nome")
     private String name;
 
+    @NotBlank
     @Pattern(regexp = "^\\S+$", message = "O campo [username] não deve conter espaços")
     private String username;
 
@@ -23,5 +34,8 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
