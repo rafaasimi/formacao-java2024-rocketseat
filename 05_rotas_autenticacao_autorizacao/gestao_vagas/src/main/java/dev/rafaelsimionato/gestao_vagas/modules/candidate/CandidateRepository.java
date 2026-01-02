69e8775c618a3eda327dc0fafa966e2e5fea743f
Rafael Simionato <rafaasimi@hotmail.com>
2026-01-02 11:01:54 -0300

    aula: criando repositorio

---
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,7 @@
+package dev.rafaelsimionato.gestao_vagas.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> { }
--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package dev.rafaelsimionato.gestao_vagas.modules.candidate.controllers;
 
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateEntity;
+import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @PostMapping("/novo")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidato: " + candidate.getName() + " criado com sucesso!");
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @PostMapping()
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
