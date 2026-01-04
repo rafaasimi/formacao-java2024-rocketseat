523e41f41b1abefbe899c231f9d466a739f0fe78
Rafael Simionato <rafaasimi@hotmail.com>
2026-01-04 20:19:06 -0300

    aula: criando entidade company

---
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,43 @@
+package dev.rafaelsimionato.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity(name = "company")
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "^\\S+$", message = "O campo [username] não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 7, max = 16, message = "O campo [password] deve conter entre 7 e 16 caracteres")
+    private String password;
+
+    private String website;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
