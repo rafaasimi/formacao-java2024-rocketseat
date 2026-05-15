85f3e64cc636c0e98c12d39a4c1cf7b32d42a931
Rafael Simionato <rafaasimi@hotmail.com>
2026-05-15 08:42:49 -0300

    aula: validando token

--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/security/SecurityFilter.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/security/SecurityFilter.java
@@ -1,17 +1,24 @@
 package dev.rafaelsimionato.gestao_vagas.security;
 
+import dev.rafaelsimionato.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JWTProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(
@@ -19,8 +26,22 @@ public class SecurityFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
+        SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
-        System.out.println(header);
+
+        if (header != null) {
+            var subjectToken = this.jwtProvider.validateToken(header);
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
+                    Collections.emptyList());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
 
         filterChain.doFilter(request, response);
 
