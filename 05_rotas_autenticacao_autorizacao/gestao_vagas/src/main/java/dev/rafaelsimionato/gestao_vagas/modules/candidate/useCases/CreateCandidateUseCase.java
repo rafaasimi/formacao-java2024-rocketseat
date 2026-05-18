b5142dabec6cc8412e5b36deabcb89e72c66772f
Rafael Simionato <rafaasimi@hotmail.com>
2026-05-18 08:58:09 -0300

    aula: hash da senha do candidato

--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import dev.rafaelsimionato.gestao_vagas.exceptions.UserFoundException;
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateEntity;
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
 
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent(candidate -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
 
