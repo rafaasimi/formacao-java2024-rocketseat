72c0f766e4b12895fa9df88a8d5501f60332982d
Rafael Simionato <rafaasimi@hotmail.com>
2026-05-18 08:49:23 -0300

    aula: refatorando jobcontroller

--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/controllers/JobController.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,7 +1,9 @@
 package dev.rafaelsimionato.gestao_vagas.modules.company.controllers;
 
+import dev.rafaelsimionato.gestao_vagas.modules.company.dto.CreateJobDTO;
 import dev.rafaelsimionato.gestao_vagas.modules.company.entities.JobEntity;
 import dev.rafaelsimionato.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -18,7 +22,18 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public Object create(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+
+        var companyId = request.getAttribute("company_id");
+//        jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
+
+        var jobEntity = JobEntity.builder()
+                .description(createJobDTO.getDescription())
+                .benefits(createJobDTO.getBenefits())
+                .level(createJobDTO.getLevel())
+                .companyId(UUID.fromString(companyId.toString()))
+                .build();
+
         try {
             var result = createJobUseCase.execute(jobEntity);
             return ResponseEntity.ok().body(result);
--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/entities/JobEntity.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/entities/JobEntity.java
@@ -2,14 +2,20 @@ package dev.rafaelsimionato.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
 @Entity(name = "job")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobEntity {
 
     @Id
