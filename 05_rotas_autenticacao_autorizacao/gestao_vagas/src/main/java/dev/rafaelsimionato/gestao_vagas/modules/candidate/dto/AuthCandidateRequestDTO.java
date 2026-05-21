042bff0e656e23bbf2edfe7e36c6fcd1acdaefc3
Rafael Simionato <rafaasimi@hotmail.com>
2026-05-21 08:40:34 -0300

    aula: criando token de candidato

---
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,4 @@
+package dev.rafaelsimionato.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+}
--- 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ 05_rotas_autenticacao_autorizacao/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -17,7 +17,7 @@ import java.time.Instant;
 @Service
 public class AuthCompanyUseCase {
 
-    @Value("${security.token.secret}")
+    @Value("${security.token.secret.company}")
     private String secretKey;
 
     @Autowired
