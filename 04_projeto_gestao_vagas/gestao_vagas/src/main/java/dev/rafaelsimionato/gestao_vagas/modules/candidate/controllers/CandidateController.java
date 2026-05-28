57543aebcfb40d1a1fc3e0c2c52a3a3c67d35864
Rafael Simionato <rafaasimi@hotmail.com>
2026-05-28 15:35:29 -0300

    aula: usecase listalljobsbyfilter

--- 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,7 +2,9 @@ package dev.rafaelsimionato.gestao_vagas.modules.candidate.controllers;
 
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateEntity;
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import dev.rafaelsimionato.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import dev.rafaelsimionato.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +24,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -43,6 +49,12 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/jobs")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 
 }
--- 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/repositories/JobRepository.java
+++ 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -3,6 +3,13 @@ package dev.rafaelsimionato.gestao_vagas.modules.company.repositories;
 import dev.rafaelsimionato.gestao_vagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface JobRepository extends JpaRepository<JobEntity, UUID> { }
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    // contains - LIKE
+    // Select * from job where description like %filter%
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String description);
+
+}
