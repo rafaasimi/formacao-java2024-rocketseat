166e30a846f76a910af725120d82b7908476e887
Rafael Simionato <rafaasimi@hotmail.com>
2026-06-08 07:48:47 -0300

    aula: teste validando se o job nao existe

--- 04_projeto_gestao_vagas/gestao_vagas/src/test/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ 04_projeto_gestao_vagas/gestao_vagas/src/test/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,8 @@
 package dev.rafaelsimionato.gestao_vagas.modules.candidate.useCases;
 
+import dev.rafaelsimionato.gestao_vagas.exceptions.JobNotFoundException;
 import dev.rafaelsimionato.gestao_vagas.exceptions.UserNotFoundException;
+import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateEntity;
 import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateRepository;
 import dev.rafaelsimionato.gestao_vagas.modules.company.repositories.JobRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplyJobCandidateUseCaseTest {
@@ -33,5 +39,23 @@ public class ApplyJobCandidateUseCaseTest {
             assertThat(ex).isInstanceOf(UserNotFoundException.class);
         }
     }
+
+    @Test
+    @DisplayName("Não deve ser possivel aplicar em uma vaga que não existe")
+    public void shouldNotBeAbleToApplyJobWithJobNotFound() {
+        var idCandidate = UUID.randomUUID();
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(this.candidateRepository.findById(idCandidate))
+                .thenReturn(Optional.of(candidate));
+
+        try {
+            this.applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception ex) {
+            assertThat(ex).isInstanceOf(JobNotFoundException.class);
+        }
+    }
+
 
 }
