ef6d33c3b2822a8000d506897303a49c74ee5141
Rafael Simionato <rafaasimi@hotmail.com>
2026-06-08 08:01:02 -0300

    aula: criando entidade de applyjob

---
+++ 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/entities/ApplyJobEntity.java
@@ -0,0 +1,43 @@
+package dev.rafaelsimionato.gestao_vagas.modules.candidate.entities;
+
+import dev.rafaelsimionato.gestao_vagas.modules.candidate.CandidateEntity;
+import dev.rafaelsimionato.gestao_vagas.modules.company.entities.JobEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "apply_jobs")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyJobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id", insertable = false, updatable = false)
+    private CandidateEntity candidateEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", insertable = false, updatable = false)
+    private JobEntity jobEntity;
+
+    @Column(name = "candidate_id")
+    private UUID candidateId;
+
+    @Column(name = "job_id")
+    private UUID jobId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
---
+++ 04_projeto_gestao_vagas/gestao_vagas/src/main/java/dev/rafaelsimionato/gestao_vagas/modules/candidate/repositories/ApplyJobRepository.java
@@ -0,0 +1,9 @@
+package dev.rafaelsimionato.gestao_vagas.modules.candidate.repositories;
+
+import dev.rafaelsimionato.gestao_vagas.modules.candidate.entities.ApplyJobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ApplyJobRepository extends JpaRepository<ApplyJobEntity, UUID> {
+}
